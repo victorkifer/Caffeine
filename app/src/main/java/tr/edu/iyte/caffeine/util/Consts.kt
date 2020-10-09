669507467fbf2204d45d3d427aecc667e77aae3a
Viktor Kifer <viktor.kifer.dev@gmail.com>
2020-10-09 16:36:09 +0300

    Fixed warning on WakeLock tag name

--- app/src/main/java/tr/edu/iyte/caffeine/util/Consts.kt
+++ app/src/main/java/tr/edu/iyte/caffeine/util/Consts.kt
@@ -1,4 +1,6 @@
 package tr.edu.iyte.caffeine.util
 
+import tr.edu.iyte.caffeine.BuildConfig
+
 const val NOTIFICATION_CHANNEL_ID = "caffeine_channel"
-const val WAKE_LOCK_TAG = "CaffeineWL"
+const val WAKE_LOCK_TAG = BuildConfig.APPLICATION_ID + ":CaffeineWL"
